2efff582071deab14e22eeeb4c483a057ac6cbc8
Luis Dietl <interruptedThread@gmail.com>
2023-11-13 15:57:31 +0100

    Some changes

--- src/main.rs
+++ src/main.rs
@@ -77,31 +77,104 @@ impl<'a> Lexer<'a>
 
 }
 
+struct Parser<'a>
+{
+    lexer: Lexer<'a>,
+    curr: Token,
+    peek: Token,
+}
 
-fn process_file(path: &str) -> Result<(), io::Error>
+impl<'a> Parser<'a>
+{
+
+    fn of(mut lexer: Lexer<'a>) -> Self
+    {
+        let peek = lexer.next_token();
+        let curr = lexer.next_token();
+        Parser {
+            lexer,
+            peek,
+            curr,
+        }
+    }
+
+    fn next(&mut self)
+    {
+        self.curr = std::mem::replace(&mut self.peek, Token::Eof);
+        self.peek = self.lexer.next_token();
+    }
+
+    fn parse_header(&mut self, value: u8, buf: &mut str) 
+    {
+        self.next();
+    }
+
+    fn parse(&mut self) -> String
+    {
+        let mut buf = String::new();
+        loop {
+            println!("{:?}", self.curr);
+            match &self.curr {
+                Token::Eof => break,
+                Token::Heading(value) => self.parse_header(*value, &mut buf),
+                Token::Text(start, size) => {
+                    let slice = &self.lexer.buf[*start..*start + *size];
+                    let str = std::str::from_utf8(slice).unwrap();
+                    buf += str;
+                    self.next();
+                },
+                Token::WhiteSpace => {
+                    buf += " ";
+                    self.next();
+                },
+                Token::NewLine => {
+                    buf += "\n";
+                    self.next();
+                },
+                _ => self.next(),
+            }
+        }
+
+        buf
+    }
+
+}
+
+fn read_file(path: &str) -> Result<Vec<u8>, io::Error>
 {
     let mut file = File::open(path)?;
     let len = file.seek(io::SeekFrom::End(0))?;
     let mut buf: Vec<u8> = Vec::with_capacity(len as usize);
     file.seek(io::SeekFrom::Start(0))?;
     file.read_to_end(&mut buf)?;
+    Ok(buf)
+}
 
-    let mut lexer = Lexer::of(&buf);
-    loop {
-        let token = lexer.next_token();
-        println!("{:?}", token);
+fn process_file(path: &str, template: &str) -> Result<(), io::Error>
+{
+    let buf = read_file(path)?;
 
-        if token == Token::Eof {
-            break;
-        }
-    }
-    
+    let lexer = Lexer::of(&buf);
+    let mut parser = Parser::of(lexer);
+    let content = parser.parse();
+
+    let output = template.replace("<!-- Content -->", &content);
+    println!("{}", output);
+
     Ok(())
 }
 
 fn main()
 {
-    if let Err(err) = process_file("test.md") {
+    let template = match read_file("template.html") {
+        Ok(template) => String::from_utf8(template).unwrap(),
+        Err(err) => {
+            println!("Failed to load template: {:?}", err);
+            return;
+        }
+    };
+
+    if let Err(err) = process_file("test.md", &template) {
         println!("Failed to process file, {:?}", err);
     }
 }
--- test.md
+++ test.md
@@ -1,5 +1,4 @@
 # Test.md
-
 ## Heading 2
 ### Heading 3
 Hello world?
