4878e9241d856950251460aff7b1acc954714dfe
Luis Dietl <interruptedThread@gmail.com>
2023-11-13 11:40:28 +0100

    Imlemented text lexing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,82 @@ use std::io::{self, Seek, Read};
 
 use std::fs::File;
 
+#[derive(PartialEq, Debug)]
+enum Token 
+{
+    Heading(u8),
+    NewLine,
+    WhiteSpace,
+    Text(usize, usize),
+    Eof,
+}
+
+struct Lexer<'a> 
+{
+    buf: &'a Vec<u8>,
+    pos: usize,
+}
+
+impl<'a> Lexer<'a>
+{
+
+    fn of(buf: &'a Vec<u8>) -> Self {
+        Lexer {buf, pos: 0}
+    }
+
+    fn next_token(&mut self) -> Token
+    {
+        if self.pos >= self.buf.len() {
+            return Token::Eof;
+        }
+
+        let c = self.buf[self.pos] as char;
+        let token = match c {
+            '#' => {
+                let mut counter: u8 = 0;
+                loop {
+                    if self.pos >= self.buf.len() || 
+                        self.buf[self.pos] as char != '#' {
+                        break;
+                    }
+                    self.pos += 1;
+                    counter += 1;
+                }
+                Token::Heading(counter)
+            },
+            ' ' => {
+                self.pos += 1;
+                Token::WhiteSpace
+            },
+            '\n' => {
+                self.pos += 1;
+                Token::NewLine
+            },
+            _ => {
+                let start = self.pos;
+                let mut len = 0;
+                loop {
+                    if self.pos >= self.buf.len() {
+                        break;
+                    }
+                    let c = self.buf[self.pos] as char;
+                    if c.is_alphanumeric() || c.is_ascii_punctuation() {
+                        self.pos += 1;
+                        len += 1;
+                    } else  {
+                        self.pos += 1;
+                        break;
+                    }
+                }
+                Token::Text(start, len)
+            },
+        };
+        token
+    }
+
+}
+
+
 fn process_file(path: &str) -> Result<(), io::Error>
 {
     let mut file = File::open(path)?;
@@ -9,9 +85,17 @@ fn process_file(path: &str) -> Result<(), io::Error>
     let mut buf: Vec<u8> = Vec::with_capacity(len as usize);
     file.seek(io::SeekFrom::Start(0))?;
     file.read_to_end(&mut buf)?;
-    // for i in 0..buf.len() {
-    //     println!("{}, {}", buf[i], buf[i] as char);
-    // }
+
+    let mut lexer = Lexer::of(&buf);
+    loop {
+        let token = lexer.next_token();
+        println!("{:?}", token);
+
+        if token == Token::Eof {
+            break;
+        }
+    }
+    
     Ok(())
 }
 
--- test.md
+++ test.md
@@ -1,2 +1,5 @@
 # Test.md
+
+## Heading 2
+### Heading 3
 Hello world?
